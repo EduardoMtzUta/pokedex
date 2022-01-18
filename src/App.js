dd89f6d2b54e74068568f5b1e7ada5aa78a3d764
eduardo <190091@utags.edu.mx>
2022-01-18 17:57:01 -0600

    Cambios hechos por Juan Eduardo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import Modal from "./Modal";
+import "./App.css";
+import { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function App() {
+  const [pokemon, setPokemon] = useState({});
+
+  const [active, setActive] = useState(false);
+
+  const [counter, setCounter] = useState(1);
+
+  const toggle = () => {
+    setActive(!active);
+  };
+
+  const fetchPokemon = (id) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  };
+
+  const getRandomInt = (min = 1, max = 600) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const getNext = (id) => {
+    return (id = pokemon.id + 1);
+  };
+
+  const getBack = (id) => {
+    return (id = pokemon.id - 1);
+  };
+
+  const showAbilities = () => {
+    document.getElementById("abilities").style.display = "block";
+    setTimeout(() => {
+      document.getElementById("abilities").style.display = "none";
+    }, 10000);
+  };
+
+  useEffect(() => {
+    console.log({ pokemon });
+  }, [pokemon]);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className="flex-container">
+          <img
+            src={
+              pokemon?.sprites?.front_default ??
+              "https://1000marcas.net/wp-content/uploads/2020/01/Logo-Pokemon.png"
+            }
+            className="poke-image"
+            alt="logo"
+          />
+          <img
+            src={
+              pokemon?.sprites?.back_default ??
+              "https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png"
+            }
+            className="poke-image"
+            alt="logo"
+          />
+        </div>
+        <p>Id: {pokemon.id ?? "No pokemon selected"}</p>
+        <p>Name: {pokemon.name ?? "No pokemon selected"}</p>
+        <button
+          className="hiddenAbilities btn btn-outline-primary"
+          onClick={(showAbilities, toggle)}
         >
-          Learn React
-        </a>
+          Abilities
+        </button><br/><br/>
+        <div id="abilities">
+          <Modal active={active} toggle={toggle}>
+            <h3 className="TextAbilitiesTitle">Abilities of {pokemon.name}</h3>
+            <br />
+            {JSON.stringify(pokemon.abilities)}
+          </Modal>
+        </div>
+        <div className="flex-container">
+          <button
+            className="button"
+            onClick={() => fetchPokemon(getBack()) ?? fetchPokemon(600)}
+          >
+            Back
+          </button>
+          <button
+            className="button"
+            onClick={() => fetchPokemon(getRandomInt())}
+          >
+            Random
+          </button>
+          <button
+            className="button"
+            onClick={() => fetchPokemon(getNext()) ?? fetchPokemon(1)}
+          >
+            Next
+          </button>
+        </div>
       </header>
     </div>
   );
